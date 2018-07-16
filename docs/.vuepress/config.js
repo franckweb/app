97fc0447c178b36b87bbec13547e69fa88d94ade
rijkvanzanten <rijkvanzanten@me.com>
2018-07-16 15:47:16 +0200

    Add nav

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,12 +5,39 @@ module.exports = {
   title: 'Directus',
   description: 'A headless CMS that manages your content, not your workflow.',
   themeConfig: {
-    // NOTE: For now, we use the github.com links as long as the docs aren't live yet
     nav: [
-      { text: 'Home', link: 'https://directus.github.io/app' },
+      { text: 'Home', link: '/' },
+      { text: 'User Guide', link: '/user-guide/' },
+      { text: 'Developer Guide', link: '/developer-guide/' },
+      { text: 'Contributors Guide', link: '/contributors-guide/' },
     ],
     repo: 'directus/app',
     docsDir: 'docs',
     editLinks: true,
+    sidebar: {
+      '/user-guide/': [
+        '',
+      ],
+
+      '/developer-guide/': [
+        '',
+      ],
+
+      '/contributors-guide/': [
+        '',
+        'working-with-github',
+        'setting-up-local-dev-env',
+        'translations-and-title-format',
+        'using-the-api'
+      ],
+
+      // fallback
+      '/': [
+        '',
+        'user-guide',
+        'developer-guide',
+        'contributors-guide',
+      ]
+    }
   }
 };
